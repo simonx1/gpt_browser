acec514f465f7b8458c3ecc5a6b5466bf17daed5
Szymon Kurcab <szymon.kurcab@gmail.com>
2023-07-27 11:18:39 -0400

    Updated prompt

--- page_analyzer.rb
+++ page_analyzer.rb
@@ -7,7 +7,7 @@ class PageAnalyzer
 
   def analyze
       qq = <<-QUERY
-      Here is an HTML page. I would like you to examine its content and generate a list of HTML elements such as link, button, input, div, li, etc. Each pair in the list should consist of a CSS selector that identifies the HTML element as the first item, followed by a brief one-sentence summary describing the function or purpose of the element as the second item. An example pair might look like this: [".div a", "Login link"]. Prefer using "id" atribute as a selector. Please refrain from adding any additional text or commentary beyond this structure.
+      Here is an HTML page. I would like you to examine its content and generate a list of HTML elements such as link, button, input, div, li, etc. Each pair in the list should consist of a CSS selector that identifies the HTML element as the first item, followed by a brief one-sentence summary describing the function or purpose of the element as the second item. An example pair might look like this: [".div a", "Login link"]. If "id" is present use it as the selector. Please refrain from adding any additional text or commentary beyond this structure.
       QUERY
 
     OpenAIGPT4.new(qq).query(prompt: @page_content)
