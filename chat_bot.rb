7fa87afec3496c24ad2bdd0fea492c1dadad02db
Szymon Kurcab <szymon.kurcab@gmail.com>
2023-07-28 06:18:26 -0400

    More robust elements cleanup

--- chat_bot.rb
+++ chat_bot.rb
@@ -6,6 +6,9 @@ require_relative 'openai_gpt4'
 require 'pry'
 
 class ChatBot
+
+  attr_reader :driver
+
   def initialize
     @driver = Selenium::WebDriver.for :chrome
     @openai_gpt4 = OpenAIGPT4.new("You are a Ruby developer. Please write Ruby code that uses Selenium gem do \ 
@@ -15,38 +18,61 @@ No other text, just Ruby code. @driver is a Selenium::WebDriver instance.")
   end
 
   def navigate_to(url)
-    @driver.navigate.to url
+    driver.navigate.to url
   end
 
-  def sanitize_page
+  def sanitize_page(strict: true)
     # Get the page source
-    source = @driver.page_source
+    source = driver.page_source
 
     # Use Nokogiri to parse the source
-    doc = Nokogiri::HTML(source)
+    html_doc = Nokogiri::HTML(source)
 
-    # Remove all script and style tags
-    doc.xpath('//script').remove
-    doc.xpath('//style').remove
-    doc.xpath('//svg').remove
-    doc.xpath('//head').remove
-    doc.xpath('//iframe').remove
-    doc.xpath('//link').remove
+    html_doc  = remove_redundant_elements(html_doc)
+    html_doc  = remove_non_actionable_elements(html_doc) if strict == true
 
     # Get the HTML content without script and style tags
-    puts "\n\n\n #{doc.to_html}\n\n"
-    doc.to_html
+    puts "\n\n\n #{html_doc.to_html}\n\n"
+    html_doc.to_html
+  end
+
+  def remove_redundant_elements(html_doc)
+    # Remove all script and style tags
+    xpath = '//script | //style | //svg | //head | //iframe | //link'
+    html_doc.xpath(xpath).remove
+    html_doc
+  end
+
+  def remove_non_actionable_elements(html_doc)
+    # Define the XPath for actionable elements.
+    xpath = '//html | //body | //a | //input | //button | //*[@tabindex or @role or @onclick or @onmousedown or @onmouseup or @onkeydown or @onkeyup or @onkeypress]'
+  
+    # Get actionable elements.
+    actionable_elements = html_doc.xpath(xpath)
+  
+    # Get all elements and convert to array.
+    all_elements = html_doc.xpath('//*').to_a
+  
+    # Calculate non-actionable elements by subtracting actionable elements from all elements.
+    non_actionable_elements = all_elements - actionable_elements
+  
+    # Remove non-actionable elements from the HTML document only if they don't contain any actionable elements.
+    non_actionable_elements.each do |element|
+      element.remove if (element.xpath('.//*') & actionable_elements).empty?
+    end
+  
+    html_doc
   end
 
   def analyze_page
     page = PageAnalyzer.new(sanitize_page).analyze
-    page = @driver.find_element(tag_name: 'body').text if page.nil?
+    page = driver.find_element(tag_name: 'body').text if page.nil?
     puts "Page analysis or text content:\n\n #{page}\n\n"
     page
   end
 
   def execute_action(action)
-    page_content = @driver.page_source
+    page_content = driver.page_source
     ActionExecutor.new(page_content, action).execute
   end
 
@@ -82,7 +108,7 @@ No other text, just Ruby code. @driver is a Selenium::WebDriver instance.")
         binding.pry
         next
       elsif command.downcase == 'exit'
-        @driver.quit
+        driver.quit
         exit-program
       end
 
